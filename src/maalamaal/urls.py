4bbe1cf76043a847483a14c14d153321dbc8bd52
desertSniper87 <torsho@localhost.localdomain>
2017-12-22 23:58:14 +0600

    fucking auth

--- src/maalamaal/urls.py
+++ src/maalamaal/urls.py
@@ -16,6 +16,24 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import hello_world, home_page, about_page, contact_page, login_page, register_page
+
 urlpatterns = [
+    url(r'^$', home_page),
     url(r'^admin/', admin.site.urls),
+    url(r'^about/$', about_page),
+    url(r'^contact/$', contact_page),
+    url(r'^login/$', login_page),
+    url(r'^register/$', register_page),
+    url(r'^hello/$', hello_world),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    # + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# urlpatterns = [
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/maalamaal/views.py
+++ src/maalamaal/views.py
@@ -1,6 +1,78 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, get_user_model
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
+    context = {
+               "title" : "Home Page",
+               "content" : "Welcome to the web page",
+              }
+    if request.user.is_authenticated is True:
+        context["premium_content"] = "YEEEEEAAAAAHHHHHH"
+
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    form = ContactForm(request.POST or None)
+    context = {
+               "title" : "Contact",
+               "content" : "Welcome to the Contact page", 
+               "form" : form
+              }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+        # print(request.POST.get('form_full_name'))
+        # print(request.POST.get('form_email'))
+        # print(request.POST.get('form_content'))
+    return render(request, "contact/view.html", context)
+
+def about_page(request):
+    context = {
+               "title" : "About",
+               "content" : "Welcome to the about page"
+              }
+    return render(request, "home_page.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+               "form" : form
+              }
+    print("User logged in")
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        print (form.cleaned_data)
+        user = authenticate(username=username, password=password)
+        # print(user)
+        if request.user.is_authenticated is True:
+            print("User is authenticated")
+        if user is not None:
+            login(request, user)
+            context['form'] = LoginForm()
+            return redirect('/login')
+        else:
+            print("ERROR")
+
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+               "form" : form
+              }
+    if form.is_valid():
+        print (form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+        # print(user)
+    return render(request, "auth/register.html", context)
+
+def hello_world(request):
     return HttpResponse("Hello world")
