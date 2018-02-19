e81aad9d1fd5e9baac5e967064c8c0b62315a00d
desertSniper87 <torsho@localhost.localdomain>
2018-02-19 12:21:50 +0600

    deleted ignored

--- src/carts/models.py
+++ src/carts/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 from products.models import Product
 from django.conf import settings
+
 User = settings.AUTH_USER_MODEL
 
 class Cart(models.Model):
@@ -16,4 +17,7 @@ class Cart(models.Model):
     total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(str):
+        return str(self.id)
     
