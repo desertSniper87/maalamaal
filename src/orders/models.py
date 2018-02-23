826fcff0df8c90dc7a670fb8727d51ac4d471c39
desertSniper87 <torsho@localhost.localdomain>
2018-02-23 13:44:15 +0600

    update order

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author            : desertsniper87 <torshobuet@gmail.com>
-# Date              : 12.02.2018
-# Last Modified Date: 12.02.2018
+# Date              : 23.02.2018
+# Last Modified Date: 23.02.2018
 from django.db import models
 
-# from products.models import Product
 from carts.models import Cart
-# User = settings.AUTH_USER.MODEL
+
+ORDER_STATUS_CHOICES = (
+                        ('created', 'Created'),
+                        ('paid', 'Paid'),
+                        ('shipped', 'Shipped'),
+                        ('refunded', 'Refunded'), 
+                       )
 
 class Order(models.Model):
     order_id         = models.CharField(max_length = 120)
     cart             = models.ForeignKey(Cart, on_delete='Cascade')
     status           = models.CharField(max_length=120, default='created')
+    order_total      = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.order_id
