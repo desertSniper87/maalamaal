8f52a66d480b9673108f8fc760232854dd10107c
desertSniper87 <torsho@localhost.localdomain>
2018-02-23 21:48:55 +0600

    order model created

--- src/orders/models.py
+++ src/orders/models.py
@@ -4,7 +4,7 @@
 # Date              : 23.02.2018
 # Last Modified Date: 23.02.2018
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from carts.models import Cart
 from maalamaal.utils import unique_order_id_generator
@@ -21,13 +21,45 @@ class Order(models.Model):
     cart             = models.ForeignKey(Cart, on_delete='Cascade')
     status           = models.CharField(max_length=120, default='created')
     order_total      = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    shipping_total   = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 
     def __str__(self):
         return self.order_id
+
+    def update_total(self):
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total +  shipping_total
+        self.order_total = new_total
+        self.save()
+        return new_total
+
+
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
-    
+
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:                     #The sender is Cart. Cart pre-exist. So we find the cart.
+        cart_obj = instance             
+        cart_total = cart_obj.total
+        cart_id = cart_obj.id
+
+        qs = Order.objects.filter(cart__id=cart_id)
+
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+def post_save_new_cart_total(sender, instance, created, *args, **kwargs):
+    if created:                          # No cart existed
+        instance.update_total()
+
+post_save.connect(post_save_new_cart_total, sender=Cart)
+
+
