56e994abeabaf0702593b5d2db1ed3c4a3b19615
desertSniper87 <torsho@localhost.localdomain>
2018-02-25 13:11:14 +0600

    prepare for deploy to heroku using procfile and runtime

--- src/maalamaal/settings/production.py
+++ src/maalamaal/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't)8=q%+97=k7gxk5v6*2dlr@50&4hz5isx%tdj6)9v8x4z$q)q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapps.com']
 
 
 # Application definition
