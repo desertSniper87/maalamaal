58d4f7aab6873925538ef19ae031341b270c6f39
desertSniper87 <torsho@localhost.localdomain>
2018-02-25 13:20:01 +0600

    changed production database in settings file

--- src/maalamaal/settings/production.py
+++ src/maalamaal/settings/production.py
@@ -83,6 +83,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
